9d8698a145c25d412e6414b48a0dfb5d7fca6c8b
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-10-06 20:38:49 +0300

    commit tree f2cd81be9f9fb1c5c9ae4af72f7114efd7cb0422

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+     has_secure_password
+
+    def authenticate(password)
+        salt = password_digest[0..28]
+        return false unless BCrypt::Engine::hash_secret(password, salt) == self.password_digest
+        self
+    end
+end
